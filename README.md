728bb873e6c901fe59db39f23a8038608a32deaa
Nicolas <nicolas.pineda1019@outlook.com>
2022-11-19 17:23:12 -0500

    added querys and mutations

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # graphql-server
+Querys examples
+query getQuery( $courseId: Int!, $topicSearch: String  ){
+  course(id: $courseId){
+    id,
+    topic
+  }
+  courses(topic: $topicSearch){
+    id,
+  }
+}
+query getTopic( $topicSearch: String,  ){
+  courses(topic: $topicSearch){
+    id,
+  } 
+}
+
+query getCoursesWithFragments( $courseId: Int!, $courseId2: Int! ){
+  coruse1: course(id: $courseId){
+    ...courseFields
+  }
+  course2: course(id: $courseId2){
+    ...courseFields
+  }
+}
+fragment courseFields on Course {
+  tittle
+  topic
+}
+Mutation examples
+mutation updateCourse($courseId: Int!, $topicSearch: String!,) {
+  updateCourseTopic(id: $courseId, topic: $topicSearch){
+    ...courseFields
+  }
+}
--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const { graphqlHTTP }  = require('express-graphql');
 const { buildSchema } = require('graphql');
+const { courses } = require('./data.json');
 
 const schema = buildSchema(`
     type Query {
         message: String
+        course(id: Int!): Course
+        courses(topic: String): [Course] 
+    }
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    }
+    type Course {
+        id: Int
+        tittle: String
+        topic: String
+        url: String
     }
 `);
+
+const getCourse = (args) => {
+    const { id } = args;
+    return courses.filter( course => course.id === id)[0];
+}
+const getCourses = (args) => {
+    const { topic } = args;
+    if ( !!topic ) return courses.filter( course => course.topic === topic);
+    return courses;
+}
+const updateCourseTopic = ( {id, topic}) => {
+    courses.map( course => {
+        if (course.id === id ){
+            course. topic = topic
+            return course;
+        };
+    })
+    return courses.filter( course => course.id === id)[0];
+}
 const root = {
-    message: () => "hola mundo"
+    message: () => "hola mundo",
+    course: getCourse,
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
 }
 const app = express();
 app.use('/graphql', graphqlHTTP({
